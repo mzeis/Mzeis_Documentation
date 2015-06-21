9bd9240000db84bae3948b51fd723fe05b6f892a
Matthias Zeis <admin@matthias-zeis.com>
2015-06-21 11:20:51 +0200

    Add backend controller

---
+++ src/app/code/community/Mzeis/Documentation/controllers/Adminhtml/Mzeis/DocumentationController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Mzeis_Documentation_Adminhtml_Mzeis_DocumentationController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
