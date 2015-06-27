26077786321919d572c5058746fa409265419494
Matthias Zeis <admin@matthias-zeis.com>
2015-06-27 17:04:22 +0200

    Add link to system configuration to sidebar

--- src/app/code/community/Mzeis/Documentation/Block/Adminhtml/Page/Sidebar.php
+++ src/app/code/community/Mzeis/Documentation/Block/Adminhtml/Page/Sidebar.php
@@ -3,6 +3,15 @@
 class Mzeis_Documentation_Block_Adminhtml_Page_Sidebar extends Mage_Adminhtml_Block_Template
 {
     /**
+     * Returns the URL of the system configuration section.
+     *
+     * @return string
+     */
+    public function getConfigurationUrl() {
+        return Mage::helper("adminhtml")->getUrl('adminhtml/system_config/edit', array('section' => 'admin'));
+    }
+
+    /**
      * Returns the URL of the documentation homepage.
      *
      * @return string
